413dd97242956e2ef841fe3fbcdcb2006d896c24
unknown <kevyb07@gmail.com>
2021-03-04 22:48:00 +0200

    Back End User Authentication

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,22 +1,17 @@
-const notFound = (req, res, next) =>{
-    const error = new Error(`Not Found - ${req.originalUrl}`)
-    res.status(404)
-    next(error)
-
+const notFound = (req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`)
+  res.status(404)
+  next(error)
 }
 
+const errorHandler = (err, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode
 
-
-
-const errorHandler = (err, req, res, next ) =>{
-    const statusCode = res.statusCode === 200 ? 500 : res.statusCode
-
-    res.status(statusCode)
-    res.json({
-        message: err.message,
-        stack: process.env.NODE_ENV === 'production' ? null : err.stack,
-    })
-
+  res.status(statusCode)
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+  })
 }
 
-export {notFound, errorHandler}
+export { notFound, errorHandler }
