f12f97d79fe67127f19c5caca023e72c659c2e00
unknown <kevyb07@gmail.com>
2021-03-11 20:42:30 +0200

    Front End User Authentication and profile

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,34 +1,57 @@
 import React from 'react'
-import { LinkContainer,} from 'react-router-bootstrap'
-import { Container, Navbar, Nav } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import { LinkContainer } from 'react-router-bootstrap'
+import { Container, Navbar, Nav, NavDropdown } from 'react-bootstrap'
+import { logout } from '../actions/userActions'
 
+const Header = () => {
+  const dispatch = useDispatch()
+  const userLogin = useSelector((state) => state.userLogin)
+  const { userInfo } = userLogin
+  const logoutHander = () => {
+    dispatch(logout())
+  }
+  return (
+    <header>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <LinkContainer to='/'>
+            <Navbar.Brand>Online Shop</Navbar.Brand>
+          </LinkContainer>
 
-const header = () => {
-    return (
-        <header>
-            
-            <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
-                <Container> 
-                  <LinkContainer to='/'> 
-                    <Navbar.Brand >Online Shop</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ml-auto'>
+              <LinkContainer to='/Cart'>
+                <Nav.Link>
+                  {' '}
+                  <i className='fas fa-shopping-cart'></i> Cart
+                </Nav.Link>
+              </LinkContainer>
+
+              {userInfo ? (
+                <NavDropdown title={userInfo.name} id='username'>
+                  <LinkContainer to='/profile'>
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
                   </LinkContainer>
-  
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="ml-auto">
-      <LinkContainer to='/Cart'> 
-        <Nav.Link> <i className='fas fa-shopping-cart'></i>  Cart</Nav.Link>
-      </LinkContainer>
-      <LinkContainer to='/Login'>
-      <Nav.Link> <i className='fas fa-user'></i>  Sign In</Nav.Link>
-      </LinkContainer>
-    </Nav>
-  </Navbar.Collapse>
-  </Container>
-</Navbar>
-
-        </header>
-    )
+                  <NavDropdown.Item onClick={logoutHander}>
+                    LogOut
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <LinkContainer to='/Login'>
+                  <Nav.Link>
+                    {' '}
+                    <i className='fas fa-user'></i> Sign In
+                  </Nav.Link>
+                </LinkContainer>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  )
 }
 
-export default header
+export default Header
