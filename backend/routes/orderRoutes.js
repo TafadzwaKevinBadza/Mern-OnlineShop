c91e7f172e9069199b3e57041fa590d979d0d845
unknown <kevyb07@gmail.com>
2021-07-23 13:41:30 +0200

    Order Details

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -4,10 +4,13 @@ import {
   addOrderItems,
   getOrderById,
   updateOrdeerToPaid,
+  getMyOrders,
 } from '../controllers/orderController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 router.route('/').post(protect, addOrderItems)
+router.route('/myorders').get(protect, getMyOrders)
 router.route('/:id').get(protect, getOrderById)
 router.route('/:id/pay').put(protect, updateOrdeerToPaid)
+
 export default router
