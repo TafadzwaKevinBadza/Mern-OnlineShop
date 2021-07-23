8472385c005617bac4bdd8e0f129434e4b0d0839
unknown <kevyb07@gmail.com>
2021-07-23 11:28:21 +0200

    Paypal Payments

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -1,5 +1,4 @@
-import React from 'react'
-import { useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Button, Col, Row, Card, Image, ListGroup } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
